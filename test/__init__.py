409eae77a31e1493b129d2fab647248ca78e102b
Philippe Gnansounou <philippe.gnansounou@gmail.com>
2020-10-27 12:28:33 +0100

    Test GET /users

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
-from test import test_userView
---
+++ test_view.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_testing import TestCase
+from app import app
+from models import db
+import unittest, os
+
+class TestViews(TestCase):
+
+    # Config the app and database
+    def create_app(self):
+        app.config['TESTING'] = True
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'dbTest.sqlite')
+        db.init_app(app)
+        return app
+
+    # Check for response 200 for GET /users
+    def test_get_users(self):
+        tester = app.test_client(self)
+        response = tester.get("/users")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+if __name__ == '__main__':
+    unittest.main()
